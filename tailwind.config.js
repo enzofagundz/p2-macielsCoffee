e4023db9f874044834040fcdedac43499f3063a8
Enzo Fagundes <enzo.fagundes014@gmail.com>
2023-05-21 18:58:50 -0300

    A

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg': '#EBEEF0',
+        'bg-contrast': '#CCD4D9',
+        'brown-1': '#733729',
+        'brown-2': '#A65526',
+        'green-1': '#2F7334',
+        'black-1': '#1D1B20',
+        'black-2': '#201E23',
+        'blue-1': '#768B99'
+      }
+    },
   },
   plugins: [],
 }
