c05e653ffcb7dfb2fb6988aac62da5bd6cd8be0a
Enzo Fagundes <enzo.fagundes014@gmail.com>
2023-05-22 08:16:17 -0300

    Layout inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,7 @@ module.exports = {
     extend: {
       colors: {
         'bg': '#EBEEF0',
-        'bg-contrast': '#CCD4D9',
-        'brown-1': '#733729',
-        'brown-2': '#A65526',
-        'green-1': '#2F7334',
-        'black-1': '#1D1B20',
-        'black-2': '#201E23',
-        'blue-1': '#768B99'
+        'bg-contrast': '#CCD4D9'
       }
     },
   },
