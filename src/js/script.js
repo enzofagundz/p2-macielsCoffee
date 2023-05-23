d73e181db74561d459cf487fefabdb427669888d
Enzo Fagundes <enzo.fagundes014@gmail.com>
2023-05-23 17:22:13 -0300

    Tentativas...

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
+    $('#search').on('keyup', function () {
+        let search = $(this).val();
 
-})
+        let order = $('.order-title').attr('data-order');
+
+        $(order).each(function () {
+            console.log($(this));
+        });
+    });
+
+});
